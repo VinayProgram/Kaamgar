d1a6d4e773dc66ac97c234150195b816cb619723
VinayProgram <vinaytandale@gmail.com>
2025-06-20 10:48:29 +0530

    feat: implement kaamgar details retrieval in the registration service and controller

--- users-portal/src/kaamgar-worker-register/kaamgar.controller.ts
+++ users-portal/src/kaamgar-worker-register/kaamgar.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { KaamgarRegisterService } from './kaamgar.service';
 
 @Controller('kaamgar-register')
 export class KaamgarRegisterController {
-  constructor() {}
+  constructor(
+    private readonly kaamgarRegisterService: KaamgarRegisterService, // Replace 'any' with the actual service type
+  ) {}
   
+    @Get(':kaamgarId')
+    async getKaamgarDetails(@Param('kaamgarId') kaamgarId: string) {
+        return await this.kaamgarRegisterService.getKaamgarDetails(kaamgarId);
+    }
 }
--- users-portal/src/kaamgar-worker-register/kaamgar.service.ts
+++ users-portal/src/kaamgar-worker-register/kaamgar.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { db } from 'src/db';
+import * as schema from 'src/db/schema';
 @Injectable()
 export class KaamgarRegisterService {
     constructor() { }
-    public async healthCheck(): Promise<any> {
-        try {
-            return 'Hello :)';
-        }
-        catch (error: any) {
-            throw error
-        }
+    async getKaamgarDetails(kaamgarId: string) {
+        return await db.select().from(schema.users_login.kaamgarUsers)
     }
 }
