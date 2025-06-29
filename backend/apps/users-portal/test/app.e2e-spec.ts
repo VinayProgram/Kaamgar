e8bd3a63867709a0e1a4b9da80fcf64f8d3d8f82
VinayProgram <vinaytandale@gmail.com>
2025-06-29 22:28:14 +0530

    refactor: remove users-portal module and related files
    - Deleted the entire users-portal module including all associated files such as controllers, services, and configuration files.
    - Removed database schema definitions and migration files.
    - Cleaned up configuration files including ESLint, Nest CLI, and TypeScript configurations.
    - Eliminated test files and configurations related to the users-portal module.

--- backend/apps/users-portal/test/app.e2e-spec.ts
+++ backend/apps/users-portal/test/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { UsersPortalModule } from './../src/users-portal.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('UsersPortalController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [UsersPortalModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
